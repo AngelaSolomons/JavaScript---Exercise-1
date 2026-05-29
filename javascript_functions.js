7112464fdddfc5dc91501b7c67d3e67bd9bb411d
Angela Solomons <angelasolomons03@gmail.com>
2026-05-29 12:58:21 +0200

    Updated js file

--- javascript_functions.js
+++ javascript_functions.js
@@ -4,15 +4,49 @@ function multiply(a, b, c) {
 }
 console.log(`Sum of 4, 6, 12: ${multiply(4, 6, 12)}`);
 
+// Create function that converts minutes to seconds and returns the result
 let convertToSeconds = function(minutes) {
     return minutes * 60;
 }
+console.log(`5 minutes in seconds: ${convertToSeconds(5)}`);
 
+// Create function that converts Fahrenheit to Celsius and returns the result
 function fahrenheitToCelsius(fahrenheitTemp) {
     return (fahrenheitTemp - 32) / 1.8
 }
+console.log(`Fahrenheit to Celsius: ${fahrenheitToCelsius(80)}`);
 
+// Create function that takes a string as input and returns the string reversed
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
-console.log(reverseString("Hello World!"));
+console.log(`Reversed string: ${reverseString("Hello World!")}`);
+
+// Create function that takes a string as input and returns the number of vowels in the string
+function countVowels(str) {
+    const vowels = "aeiouAEIOU";
+    return str.split("").filter(char => vowels.includes(char)).length;
+}
+console.log(`Output: ${countVowels("Goodbye ")}`);
+
+// Create function that takes a number and returns true if the number is prime, and false otherwise
+function isPrime(num) {
+     if (num <= 1) return false;
+
+     for (let i = 2; i <= Math.sqrt(num); i++) {
+         if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(`Is 17 a prime number? ${isPrime(17)}`);
+console.log(`Is 34 a prime number? ${isPrime(34)}`);
+console.log(`Is 2 a prime number? ${isPrime(2)}`);
+console.log(`Is 99 a prime number? ${isPrime(99)}`);
+
+
+
+
+
+
